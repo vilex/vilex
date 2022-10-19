85f5fef48f089250be39f0f1e5fb98723e6c318d
wangxiaowei <espace@aliyun.com>
2022-10-19 22:33:00 +0800

    fix: hrm only current project files

--- packages/vite-vilex/src/index.js
+++ packages/vite-vilex/src/index.js
@@ -1,58 +1,70 @@
-const { VAR_PREFIX, END_OF_IMPORT_IDENTIFICATION, END_OF_FILE_IDENTIFICATION } = require("./contants");
-const { hmr } = require("./hmr");
-const { RewriteImportSpecifiers, InsertCodeEndOfFile } = require("./templates");
-const { TransformCode } = require("./transform");
-const { default: generate } = require('@babel/generator');
-const { parse } = require("@babel/parser");
+const {
+  VAR_PREFIX,
+  END_OF_IMPORT_IDENTIFICATION,
+  END_OF_FILE_IDENTIFICATION
+} = require('./contants')
+const { hmr } = require('./hmr')
+const { RewriteImportSpecifiers, InsertCodeEndOfFile } = require('./templates')
+const { TransformCode } = require('./transform')
+const { default: generate } = require('@babel/generator')
+const { parse } = require('@babel/parser')
+const { normalizePath } = require('vite')
 
 function vilexPlugin() {
-
+  const root = normalizePath(process.cwd())
 
   function transformIndexHtml(html) {
     const tags = [
       {
         tag: 'script',
-        attrs: { type: 'module'},
+        attrs: { type: 'module' },
         children: hmr,
         injectTo: 'head'
       }
     ]
-    return { html, tags}
+    return { html, tags }
   }
 
   function transform(code, id) {
-    if (!id.includes(".ts")) return;
+    if (!id.includes('.ts')) return
+    if (!id.includes(root)) return
 
-    const tfc = TransformCode(code, id, VAR_PREFIX, END_OF_IMPORT_IDENTIFICATION, END_OF_FILE_IDENTIFICATION)
-    
+    const tfc = TransformCode(
+      code,
+      id,
+      VAR_PREFIX,
+      END_OF_IMPORT_IDENTIFICATION,
+      END_OF_FILE_IDENTIFICATION
+    )
+
     const ris = RewriteImportSpecifiers(tfc.code, tfc.specifiers, VAR_PREFIX)
-    
-    const iceof = InsertCodeEndOfFile(ris.code, tfc.specifiers)
-    
-    const result =  generate(parse(code, {
-      plugins: ['typescript'],
-      sourceType: 'module'
-    }))
 
+    const iceof = InsertCodeEndOfFile(ris.code, tfc.specifiers)
+
+    const result = generate(
+      parse(code, {
+        plugins: ['typescript'],
+        sourceType: 'module'
+      })
+    )
 
     return {
       map: result.map,
       code: iceof.code,
       vite: {
         meta: {
-          lang: "ts",
-        },
-      },
-    };
+          lang: 'ts'
+        }
+      }
+    }
   }
 
   return {
-    name: "vite:vilex",
+    name: 'vite:vilex',
     apply: 'serve',
     transform,
     transformIndexHtml
-  };
+  }
 }
-
 
 module.exports = vilexPlugin
--- packages/vite-vilex/src/transform.js
+++ packages/vite-vilex/src/transform.js
@@ -5,63 +5,73 @@ const { default: generate } = require('@babel/generator')
 const { dirname, resolve } = require('path')
 
 const images = ['.png', '.jpg', '.svg', '.mp3', '.mp4', '.gif', '.webp', '.md']
-function TransformCode(code, id, namePrefix, endOfImportIndentification, endOfFileIdentification) {
-    let importPath, programPath, localImportSpecifiers = []
-    const astfile = parse(code, { plugins: ['typescript'], sourceType: 'module' })
+function TransformCode(
+  code,
+  id,
+  namePrefix,
+  endOfImportIndentification,
+  endOfFileIdentification
+) {
+  let importPath,
+    programPath,
+    localImportSpecifiers = []
+  const astfile = parse(code, { plugins: ['typescript'], sourceType: 'module' })
 
-    traverse(astfile, {
-        ImportDeclaration(path) {
-            importPath = path
+  traverse(astfile, {
+    ImportDeclaration(path) {
+      importPath = path
 
-            const value = path.node.source.value
+      const value = path.node.source.value
+      const blacklist = [
+        !value.startsWith('.'),
+        value.startsWith('/@fs/'),
+        images.some(v => value.includes(v))
+      ]
 
-            const isLocalImport = value.startsWith('.') && !images.some(v => value.includes(v))
-            if (isLocalImport) {
-                path.node.specifiers.forEach(specifier => {
-                    localImportSpecifiers.push({
-                        name: specifier.local.name,
-                        path: resolve(path.node.source.value),
-                        from: path.node.source.value
-                    })
-                })
-            }
-        },
-        Identifier(path) {
-            if (localImportSpecifiers.some(item => item.name == path.node.name)) 
-            {
-                const blacklist = [
-                    t.isImportSpecifier(path.parentPath),
-                    t.isVariableDeclarator(path.parent) && path.parentPath.node.id.name == path.node.name,
-                    t.isFunctionDeclaration(path.parentPath),
-                    t.isProperty(path.parentPath) && path.parentPath.node.value.name != path.node.name,
-                    t.isClassDeclaration(path.parentPath),
-                    t.isImportDefaultSpecifier(path.parentPath)
-                ]
-        
-                const replace = blacklist.some(bool => bool)
-                if (!replace) {
-                    // console.log(path.node.name, blacklist)
-                    path.node.name = `${namePrefix}${path.node.name}`
-                }
-            }
-        },
-        Program(path) {
-            programPath = path
+      const nolocal = blacklist.some(v => v)
+      if (!nolocal) {
+        path.node.specifiers.forEach(specifier => {
+          localImportSpecifiers.push({
+            name: specifier.local.name,
+            path: resolve(path.node.source.value),
+            from: path.node.source.value
+          })
+        })
+      }
+    },
+    Identifier(path) {
+      if (localImportSpecifiers.some(item => item.name == path.node.name)) {
+        const blacklist = [
+          t.isImportSpecifier(path.parentPath),
+          t.isVariableDeclarator(path.parent) &&
+            path.parentPath.node.id.name == path.node.name,
+          t.isFunctionDeclaration(path.parentPath),
+          t.isProperty(path.parentPath) &&
+            path.parentPath.node.value.name != path.node.name,
+          t.isClassDeclaration(path.parentPath),
+          t.isImportDefaultSpecifier(path.parentPath)
+        ]
+
+        const replace = blacklist.some(bool => bool)
+        if (!replace) {
+          // console.log(path.node.name, blacklist)
+          path.node.name = `${namePrefix}${path.node.name}`
         }
-    })
-
-    if (importPath) {
-        importPath.insertAfter(t.identifier(endOfImportIndentification))
-        programPath.node.body.push(
-            t.identifier(endOfFileIdentification)
-        )
+      }
+    },
+    Program(path) {
+      programPath = path
     }
+  })
 
-    const recode = generate(astfile).code
+  if (importPath) {
+    importPath.insertAfter(t.identifier(endOfImportIndentification))
+    programPath.node.body.push(t.identifier(endOfFileIdentification))
+  }
 
-    return { code: recode, specifiers: localImportSpecifiers }
+  const recode = generate(astfile).code
+
+  return { code: recode, specifiers: localImportSpecifiers }
 }
-
-
 
 exports.TransformCode = TransformCode
