9be426d659fbb1a08987decab59431cda1f96a89
wxw <297278806@qq.com>
2022-10-21 20:21:55 +0800

    fix: demo

--- projects/debugger/src/main.ts
+++ projects/debugger/src/main.ts
@@ -1,6 +1,29 @@
-import { createApp, div } from 'vilex'
+import { createApp, createElement, ref } from 'vilex'
 
-debugger
-const app = div(123)
+const count = ref(0)
+
+const app = createElement('div', [
+  {
+    fontFamily: 'cursive',
+    width: '200px',
+    margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+  createElement('p', [count]),
+  createElement('button', [
+    '点我+1',
+    {
+      onclick() {
+        count.value++
+      }
+    },
+    {
+      color: 'rgba(0,0,0,0.8)'
+    }
+  ])
+])
 
 createApp(app).mount('#app')
