4ae75228a1f7b46b44f27353c9ff1f1fdd3d379d
wxw <297278806@qq.com>
2022-11-04 20:57:32 +0800

    perf: remove package

--- packages/event-emitter/src/index.ts
+++
@@ -1,3 +0,0 @@
-import EventEmitter3 from 'eventemitter3'
-console.log(EventEmitter3)
-export const EventEmitter = EventEmitter3
--- packages/runtime-dom/src/index.ts
+++
@@ -1,5 +0,0 @@
-export function createElement(tag: string, textContent?: string) {
-  const element = document.createElement(tag)
-  element.textContent = textContent || ''
-  return element
-}
