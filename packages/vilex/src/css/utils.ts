4fb9f201bc4a9cd71b851bfd38ddc919bf797302
wangxiaowei <espace@aliyun.com>
2022-11-15 22:35:57 +0800

    style: 优化代码

--- packages/vilex/src/css/utils.ts
+++ packages/vilex/src/css/utils.ts
@@ -1,6 +1,5 @@
 let baseline = 100000
-export function genId(prefix = '') {
-  const id = prefix + Date.now().toString(36).slice(4) + baseline.toString(36)
+export function genId(prefix = ''): string {
   baseline++
-  return id
+  return prefix + Date.now().toString(36).slice(4) + baseline.toString(36)
 }
