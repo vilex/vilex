e46ab70f52110072cb9e5191e3a455ecedd1d9e8
wangxiaowei <espace@aliyun.com>
2022-12-01 19:02:27 +0800

    style: lint style

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  'subject-case': ['lower-case', 'start-case'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'perf',
-        'style',
-        'docs',
-        'refactor',
-        'chore',
-        'types',
-        'test',
-        'workflow',
-        'build',
-        'release'
-      ]
-    ]
+    'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'style', 'docs', 'refactor', 'chore', 'types', 'test', 'workflow', 'build', 'release']]
   }
 }
