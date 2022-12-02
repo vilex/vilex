ba5be05f5091c5ba750d2720dfbb3b8fea89173d
wangxiaowei <espace@aliyun.com>
2022-12-02 12:37:40 +0800

    refactor: unref

--- packages/vilex/src/vilex/store/unref.ts
+++ packages/vilex/src/vilex/store/unref.ts
@@ -1,4 +1,17 @@
-import { AnyObject } from '../../_types/common'
+import { isProxy } from '../../utils/isProxy'
 import { isRef } from './isRef'
 
-export const unref = (ref: AnyObject) => (isRef(ref) ? ref.value : ref)
+export function unref<T>(ref: T): T
+export function unref(ref: any) {
+  if (isRef(ref)) return ref.value
+  if (isProxy(ref)) {
+    const unrefObject: any = {}
+    for (const key in ref) {
+      if (Object.prototype.hasOwnProperty.call(ref, key)) {
+        unrefObject[key] = unref(ref[key])
+      }
+    }
+    return unrefObject
+  }
+  return ref
+}
